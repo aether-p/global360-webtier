5859b501c062ea49b1f14ccbc768986d93ba298f
aether-p <aether.pohl@gmail.com>
2025-08-24 12:13:05 +1000

    Adding terraform providers config

---
+++ providers.tf
@@ -0,0 +1,23 @@
+# Configure the required providers - Azure and Hashicorp Random 
+terraform {
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
